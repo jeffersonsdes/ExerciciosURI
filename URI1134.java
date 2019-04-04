0a57b0f98f2e1fc5d896e446050c1be6be1d8d78
jeffersonsdes <48225922+jeffersonsdes@users.noreply.github.com>
2019-04-03 22:11:44 -0300

    Add files via upload

--- URI1134.java
+++ URI1134.java
@@ -1,9 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class URI1134 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		int ContGas = 0;
+		int ContAlc = 0;
+		int ContDie = 0;
+		int tipo = sc.nextInt();
+
+		while (tipo != 4) {
+			if (tipo == 1) {
+				ContAlc++;
+			} else if (tipo == 2) {
+				ContGas++;
+			} else if (tipo == 3) {
+				ContDie++;
+			}
+			tipo = sc.nextInt();
+		}
+		System.out.println("MUITO OBRIGADO");
+		System.out.println("Alcool: " + ContAlc);
+		System.out.println("Gasolina: " + ContGas);
+		System.out.println("Diesel: " + ContDie);
+
+		sc.close();
 	}
 
 }
